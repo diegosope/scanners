e490c6a9fcff1322ed75c986467c92a2760fc7dc
diegosope <dsg.real.madrid@gmail.com>
2023-05-19 12:36:55 +0200

    port scan

---
+++ portScan.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+function ctrl_c(){
+ echo -e "\n\n[!] Saliendo..."
+ tput cnorm; exit 1 #recuperar cursor ; código ode error
+}
+
+#Ctrl+C
+trap ctrl_c SIGINT
+
+declare -a ports=( $(seq 1 65535) )
+
+function checkPort(){
+  
+  (exec 3<> /dev/tcp/$1/$2) 2>/dev/null
+
+  if [ $? -eq 0 ]; then
+    echo "[+] Host $1 - Port $2 (OPEN)"
+  fi
+
+  exec 3<&-
+  exec 3>&-
+}
+
+tput civis #ocultar cursor
+
+if [ $1 ]; then
+  for port in ${ports[@]}; do
+    checkPort $1 $port &
+  done
+else
+  echo -e "\n[!] Uso: $0 <ip-address>\n"
+fi
+
+wait 
